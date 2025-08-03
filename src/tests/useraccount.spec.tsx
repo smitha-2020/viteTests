0b11e2e54b7de9de46a87a9aa7c9bb0553f270fc
Salusfin <smitha.kamath2023@gmail.com>
2025-08-03 23:24:22 +0300

    added test cases for userAccount

--- src/tests/useraccount.spec.tsx
+++ src/tests/useraccount.spec.tsx
@@ -8,4 +8,18 @@ describe("should check the useraccount compoent", () => {
     const button = screen.getByRole("button");
     expect(button).toHaveTextContent(/edit/i);
   });
+
+  it("should check if the edit button is displayed for normal user", () => {
+    render(<UserAccount user={{ id: 1, name: "Smitha", isAdmin: false }} />);
+
+    const button = screen.queryByText(/edit/i);
+    expect(button).toBeNull();
+  });
+
+  it("should check if the username is displayed", () => {
+    render(<UserAccount user={{ id: 1, name: "Smitha", isAdmin: false }} />);
+
+    const name = screen.getByText(/smitha/i);
+    expect(name).toBeInTheDocument();
+  });
 });
